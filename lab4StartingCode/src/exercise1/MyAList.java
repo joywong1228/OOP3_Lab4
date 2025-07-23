1e8911a60f99e081c9031064d69112d390467be4
Lam Dao <lam.dao@edu.sait.ca>
2025-07-22 21:46:10 -0600

    complete exercise 1

--- lab4StartingCode/src/exercise1/MyAList.java
+++ lab4StartingCode/src/exercise1/MyAList.java
@@ -7,11 +7,62 @@ public class MyAList<E> implements Iterable<E>
 {
 	private ArrayList<E> list;
 
+	public MyAList()
+	{
+		list = new ArrayList<E>();
+	}
+
+	void add(E item)
+	{
+		list.add(item);
+	}
+
+	void addAll(MyAList<E> other)
+	{
+		for (E item : other) {
+			this.add(item);
+		}
+	}
+
+	E get(int index)
+	{
+		return list.get(index);
+	}
+
+	void remove(int index)
+	{
+		list.remove(index);
+	}
+
+	void set(int index, E item)
+	{
+		list.set(index, item);
+	}
+
+	int size()
+	{
+		return list.size();
+	}
+
+	boolean isEmpty()
+	{
+		return list.isEmpty();
+	}
+
+	Object[] toArray()
+	{
+		return list.toArray();
+	}
+
+	void clear()
+	{
+		list.clear();
+	}
+
 	@Override
 	public Iterator<E> iterator()
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return list.iterator();
 	}
 
 }
